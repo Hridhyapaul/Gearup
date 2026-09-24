a7527c4d0bb5fe39ec465516e65600af28d4ddd1
Hridhya Paul <Hpdesign2021@gmail.com>
2026-09-24 18:16:26 +0600

    Fixed: Category route issue

--- src/modules/category/category.routes.ts
+++ src/modules/category/category.routes.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { categoryController } from "./category.controller";
+import { auth } from "../../middlewares/auth";
+import { UserRole } from "../../generated/prisma/enums";
 
 
 const router = Router();
 
-router.post("/", categoryController.createCategory);
+router.post("/", auth(UserRole.ADMIN), categoryController.createCategory);
 router.get("/", categoryController.getAllCategories);
 router.get("/:categoryId", categoryController.getCategoryById);
-router.patch("/:categoryId", categoryController.updateCategory);
-router.delete("/:categoryId", categoryController.deleteCategory);
+router.patch("/:categoryId", auth(UserRole.ADMIN) , categoryController.updateCategory);
+router.delete("/:categoryId", auth(UserRole.ADMIN) , categoryController.deleteCategory);
 
 export const categoryRoutes = router;
